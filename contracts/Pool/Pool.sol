364d6a81e841f1d4493c1b57eb08f1f8d0bdd431
Aditya Garg <aditya26026@gmail.com>
2021-03-02 16:31:43 +0530

    edited event

--- contracts/Pool/Pool.sol
+++ contracts/Pool/Pool.sol
@@ -73,7 +73,7 @@ contract Pool is ERC20PresetMinterPauserUpgradeable {
         address lenderAddress
     );
     event AmountBorrowed(address borrower, uint256 amount);
-    event liquiditywithdrawn(
+    event Liquiditywithdrawn(
         uint256 amount,
         address lenderAddress
     );
@@ -272,7 +272,7 @@ contract Pool is ERC20PresetMinterPauserUpgradeable {
             _totalSupplied
         );
 
-        emit liquiditywithdrawn(_totalSupplied, lenderAddress);
+        emit Liquiditywithdrawn(_totalSupplied, lenderAddress);
     }
 
 
