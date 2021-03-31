020e352527a7d63a452417cd1d2f7d588c1497e9
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2021-04-01 01:27:52 +0530

    Updated config

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,6 +6,14 @@ require('@openzeppelin/hardhat-upgrades')
 const config = require('./config/config.json')
 const mnemonic = config['ganache']['blockchain']['mnemonic']
 
+task('accounts', 'Prints the list of accounts', async () => {
+  const accounts = await ethers.getSigners()
+
+  for (const account of accounts) {
+    console.log(await account.getAddress())
+  }
+})
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -13,9 +21,22 @@ const mnemonic = config['ganache']['blockchain']['mnemonic']
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  defaultNetwork: 'ganache',
+  defaultNetwork: 'hardhat',
   networks: {
+    hardhat: {
+      throwOnTransactionFailures: true,
+      forking: {
+        url: 'http://127.0.0.1:8545',
+      },
+      accounts: {
+        count: 20,
+        mnemonic,
+        accountsBalance: '10000000000000000000000',
+      },
+      blockGasLimit: 6000000000,
+    },
     ganache: {
+      throwOnTransactionFailures: true,
       url: 'http://127.0.0.1:8546',
       fork: 'http://127.0.0.1:8545',
       gasLimit: 6000000000,
@@ -32,5 +53,11 @@ module.exports = {
         runs: 200,
       },
     },
+  },
+  paths: {
+    sources: './contracts',
+    tests: './test',
+    cache: './cache',
+    artifacts: './artifacts',
   },
 }
