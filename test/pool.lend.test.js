22b22994db95137bf708aa639186f84ea6f536fb
ayush2843 <33235626+ayush2843@users.noreply.github.com>
2021-05-11 23:34:49 +0530

    added draft lend saving account test

--- test/pool.lend.test.js
+++ test/pool.lend.test.js
@@ -17,6 +17,8 @@ config = config['ganache']
 
 const poolCompiled = require('../build/contracts/Pool/Pool.sol/Pool.json')
 const poolTokenCompiled = require('../build/contracts/Pool/PoolToken.sol/PoolToken.json')
+const CompoundWithdrawAccount = "0x33333333deadbeefdeadbeefdeadbeefdeadbeef";
+
 
 describe.only('Pool', () => {
     before(async () => {
@@ -30,6 +32,7 @@ describe.only('Pool', () => {
             this.lender,
             this.address1,
             this.address2,
+            this.accountToDeposit,this.mockCreditLineAddress
         ] = accounts;
         this.salt = encodeUserData(config.OpenBorrowPool.salt)
 
@@ -198,6 +201,82 @@ describe.only('Pool', () => {
             expect(await this.poolToken.balanceOf(this.address1.address)).to.equal(
                 fBalance.sub(iBalance),
             )
+        })
+    })
+    describe('lend using savings account', async () => {
+        before(async () => {
+            const { pool, poolToken } = await this.deploy.deployPool(
+                this.poolFactory,
+                this.borrower,
+                ethers.constants.AddressZero,
+                ethers.constants.AddressZero
+            );
+    
+            this.pool = new ethers.Contract(
+                pool,
+                poolCompiled.abi,
+                this.borrower,
+            )
+        
+            this.poolToken = await new ethers.Contract(
+                poolToken,
+                poolTokenCompiled.abi,
+                this.admin,
+            )
+            const SavingsAccount = await ethers.getContractFactory("SavingsAccount");
+            this.savingsAccount = await SavingsAccount.connect(
+            this.proxyAdmin
+            ).deploy();
+
+            this.StrategyRegistry = await ethers.getContractFactory("StrategyRegistry");
+            this.strategyRegistry = await this.StrategyRegistry.connect(
+            this.proxyAdmin
+            ).deploy();
+
+            await this.savingsAccount.initialize(
+            this.admin.address,
+            this.strategyRegistry.address,
+            this.mockCreditLineAddress.address
+            );
+
+            const depositValueToTest = parseEther('1');
+            await this.savingsAccount
+            .connect(this.lender)
+            .depositTo(
+              depositValueToTest,
+              ethers.constants.AddressZero,
+              ethers.constants.AddressZero,
+              this.lender.address,
+              { value: depositValueToTest }
+            );
+            await this.savingsAccount
+            .connect(this.lender)
+            .approve(
+                ethers.constants.AddressZero,
+                this.pool.address,
+                parseEther('0.001')
+            );
+            
+        })
+        it('should lend using Saving Account', async () => {
+            
+            // let iBalance = await this.address1.getBalance();
+            const amountLent = parseEther('0.0001')
+            let initialBalance = await this.savingsAccount.userLockedBalance(
+                this.lender.address,
+                ethers.constants.AddressZero,
+                ethers.constants.AddressZero
+              );
+            await this.pool
+            .connect(this.lender)
+            .lend(this.lender.address, amountLent, true)
+            let finalBalance = await this.savingsAccount.userLockedBalance(
+                this.lender.address,
+                ethers.constants.AddressZero,
+                ethers.constants.AddressZero
+              );
+            expect(initialBalance.sub(finalBalance)).equal(amountLent);
+          
         })
     })
 })
